31d428e24131160a0906d664719d085f8e0cccf0
Shishir <shishir.gaire11@gmail.com>
2023-02-25 11:16:54 +0545

    socket io in same server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const helmet = require('helmet');
-const morgan = require('morgan');
+// const helmet = require('helmet');
+// const morgan = require('morgan');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -23,6 +23,15 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     next();
 });
+
+const httpServer = require('http').createServer(app);
+const options = {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+};
+const io = require('socket.io')(httpServer, options);
 
 
 app.get('/', (req, res) => {
@@ -71,8 +80,55 @@ app.use((err, req, res, next) => {
     });
 })
 
+
+
+//socket io part 
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some(user => user.userId === userId) && users.push({ userId, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId);
+}
+
+io.on("connection", (socket) => {
+
+    //after connection
+    console.log("a user is connected");
+
+    socket.on("addUser", userId => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send and get message
+    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId, text,
+        })
+    })
+
+    //after disconnection
+    socket.on("disconnect", () => {
+        console.log("a user disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+})
+
+
+
+
 const PORT = process.env.PORT || 8800;
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log("Server Api Started");
 })
