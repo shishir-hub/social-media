5730bbb14d8ab8afdd9d9baf4753a0270a930b0b
Shishir <shishir.gaire11@gmail.com>
2023-02-25 12:00:35 +0545

    socket io in same server

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-const httpServer = require('https').createServer(app);
+const httpServer = require('http').createServer(app);
 const options = {};
 const io = require('socket.io')(httpServer, options);
 
